4fd85a5fd1940b7475a9f50ffee46f450c9efefb
sakkaku <sakkaku@gmail.com>
2022-05-16 23:35:06 -0500

    Revise styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,12 @@
 
   <div class="about-area">
     <hr />
-    <h2>Benevolent Dictator of the Multiverse</h2>
+
+    <div class="description">
+      <p>Job Title: Software Engineer and Benevolent Dictator of the Multiverse</p>
+      <p>Known Aliases: sakkaku, envalid</p>
+    </div>
+
     <hr />
 
     <h3>Desocialize</h3>
@@ -32,16 +37,16 @@ h3 {
   line-height: 3rem;
 }
 
+.description {
+  padding: 1rem;
+}
+
 .inline-nav {
   display: flex;
   flex-flow: row wrap;
   margin: 1rem;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   text-align: center;
-}
-
-.inline-nav > * {
-  width: 5rem;
 }
 </style>
