65ab4578638bd559295abbf24ff9580ca45016d3
sakkaku <sakkaku@gmail.com>
2022-05-28 10:59:26 -0500

    Move site title after page title.

--- src/stores/useTitleStore.ts
+++ src/stores/useTitleStore.ts
@@ -3,22 +3,22 @@ import { defineStore } from "pinia";
 export const useTitleStore = defineStore({
   id: "title",
   state: () => ({
-    title: ''
+    title: ""
   }),
   actions: {
-    set (newTitle: string | undefined): void {
+    set(newTitle: string | undefined): void {
       const isHome = newTitle === "Home";
-      if(isHome) {
-        document.title = "Envalid - Home";
+      if (isHome) {
         this.title = "Envalid";
+        document.title = "Home - Envalid";
       } else {
         this.title = newTitle ?? "";
-        document.title = this.title == "" ? "Envalid" : "Envalid - " + this.title;
+        document.title = (this.title != "" ? (this.title + " - ") : "") + "Envalid";
       }
     },
     reset(): void {
-      this.set('');
-    },
-  },
+      this.set("");
+    }
+  }
 });
 
